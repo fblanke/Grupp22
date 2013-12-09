dd552be1edd6da55d7f9ea909cdee674acb19dd6
fblanke <felix.olof.blanke@gmail.com>
2013-12-09 23:49:39 +0100

    Lite mer falska lögner!

--- Databaser 2 - Uppgift 1/src/pushTyp.java
+++ Databaser 2 - Uppgift 1/src/pushTyp.java
@@ -3,5 +3,6 @@ public class pushTyp {
 	public static void main(String args[]){
 		System.out.println("Jag hatar git");
 		System.out.println("Jag tycker om git!");
+		System.out.println("Men lite struligt är det ändå!!!");
 	}
 }
